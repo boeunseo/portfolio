d785a640c0d0ab410a51c08d92c9568c225c7ec2
Boeun <sseobe@gmail.com>
2021-10-12 16:56:19 +0800

    JS: Home transparent

--- main.js
+++ main.js
@@ -30,6 +30,15 @@ contactmeBtn.addEventListener( 'click', () => {
   scrollIntoView('#contact');
 })
 
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY/homeHeight ;
+})
+
+
 
 function scrollIntoView(selector) {
   const scrollTo = document.querySelector(selector);
@@ -41,3 +50,4 @@ function scrollIntoView(selector) {
   })    
   // scrollTo.scrollIntoView({ behavior: "smooth" });
 }
+
