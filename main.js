a1ee9382759effaaae1f156fa98445ba73614bc1
Boeun <sseobe@gmail.com>
2021-10-20 11:11:51 +0800

    JS: Project filtering

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ navbarMenu.addEventListener('click', event => {
 
   const link = event.target.dataset.link
   if (link == null) {
-    return
+    return;
   }
 
   scrollIntoView(link); 
@@ -50,6 +50,34 @@ document.addEventListener('scroll', () => {
 // Handle click on the "Arrow up" button
 arrowUp.addEventListener( 'click', () => {
   scrollIntoView('#home');
+})
+
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects =  document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter =  e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if(filter == null) {
+    return;
+  }
+
+  projectContainer.classList.add('anim-out');
+
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+
 })
 
 
