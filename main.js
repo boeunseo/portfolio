f566f1ae41d6781df0550caf593b82c6b0185c4b
Boeun <sseobe@gmail.com>
2021-10-20 11:39:46 +0800

    JS: remove active from nav

--- main.js
+++ main.js
@@ -63,9 +63,13 @@ workBtnContainer.addEventListener('click', (e) => {
     return;
   }
 
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+  const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+  target.classList.add('selected');
+
   projectContainer.classList.add('anim-out');
-
-
   setTimeout(() => {
     projects.forEach((project) => {
       if (filter === '*' || filter === project.dataset.type) {
@@ -74,7 +78,7 @@ workBtnContainer.addEventListener('click', (e) => {
         project.classList.add('invisible');
       }
     });
-    
+
     projectContainer.classList.remove('anim-out');
   }, 300);
 
